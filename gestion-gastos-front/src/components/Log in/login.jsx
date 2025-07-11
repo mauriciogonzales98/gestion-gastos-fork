67a97bfc7559448a6d7938374a004acc08edf3a0
Tomás Montana <tomasmontania1@gmail.com>
2025-07-11 16:12:08 -0300

    Añadido ESLint y sus dependencias

---
+++ gestion-gastos-front/src/components/Log in/login.jsx
@@ -0,0 +1,14 @@
+const login = () => {
+<div>
+  <h1>Login Page</h1>
+  <form>
+    <label>Username:</label>
+    <input type="text" name="username" required />
+    <label>Password:</label>
+    <input type="password" name="password" required />
+    <button type="submit">Login</button>
+    </form>
+</div>
+
+}
+export default login;
